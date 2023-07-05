43565532af7e75547c507a4e605d19e401dd1a2c
murisdotcom <muhrizalsidik>
2023-07-05 22:18:31 +0700

    tampilan login

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -12,20 +12,49 @@
   <title>{{$title}}</title>
 
   <!-- <link rel="canonical" href="https://getbootstrap.com/docs/5.3/examples/sign-in/"> -->
-
-
-
-
-
   <link href="{{asset('css/bootstrap.min.css')}}" rel="stylesheet">
 
-  <!-- Custom styles for this template -->
-  <link href="{{asset('css/myCss/sign-in.css')}}" rel="stylesheet">
+  <link href="{{asset('css/myCdd/navbar-fixed.css')}}" rel="stylesheet">
+
 </head>
 
 <body class="d-flex align-items-center py-4 bg-body-tertiary">
 
-  @yield('container')
+  <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">Fixed navbar</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarCollapse">
+        <ul class="navbar-nav me-auto mb-2 mb-md-0">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Link</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link disabled">Disabled</a>
+          </li>
+        </ul>
+        <button class="btn btn-outline-primary"><i class="bi bi-person-circle"> Logout</i></button>
+        <!-- <ul class="dropdown-menu gap-1 p-2 position-relative rounded-3 mx-0 shadow w-220px" data-bs-theme="light">
+          <li><a class="dropdown-item rounded-2 active" href="#">Action</a></li>
+          <hr class="dropdown-divider">
+          <li><a class="dropdown-item rounded-2" href="#"><i class="bi bi-chat-left"> Logout</i></a></li>
+        </ul> -->
+        <!-- <form class="d-flex" role="search">
+          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+          <button class="btn btn-outline-success" type="submit">Search</button>
+        </form> -->
+      </div>
+    </div>
+  </nav>
+
+  <div class="container-fluid mt-5">
+    @yield('container')
+  </div>
 
   <script src="{{asset('js/bootstrap.bundle.min.js')}}"></script>
 
--- resources/views/login/index.blade.php
+++ resources/views/login/index.blade.php
@@ -1,26 +1,54 @@
-@extends('layouts.main')
-@section('container')
-<main class="form-signin w-100 m-auto">
-  <form>
-    <h1 class="h3 mb-3 fw-normal text-center">Please sign in</h1>
+<!doctype html>
+<html lang="en" data-bs-theme="auto">
 
-    <div class="form-floating mb-3">
-      <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com" autofocus>
-      <label for="floatingInput">Email address</label>
-    </div>
-    <div class="form-floating">
-      <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
-      <label for="floatingPassword">Password</label>
-    </div>
+<head>
+  <!-- <script src="../assets/js/color-modes.js"></script> -->
 
-    <!-- <div class="form-check text-start my-3">
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="description" content="">
+  <!-- <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+  <meta name="generator" content="Hugo 0.112.5"> -->
+  <title>{{$title}}</title>
+
+  <!-- <link rel="canonical" href="https://getbootstrap.com/docs/5.3/examples/sign-in/"> -->
+
+
+
+
+
+  <link href="{{asset('css/bootstrap.min.css')}}" rel="stylesheet">
+
+  <!-- Custom styles for this template -->
+  <link href="{{asset('css/myCss/sign-in.css')}}" rel="stylesheet">
+</head>
+
+<body class="d-flex align-items-center py-4 bg-body-tertiary">
+  <main class="form-signin w-100 m-auto">
+    <form>
+      <h1 class="h3 mb-3 fw-normal text-center">Please sign in</h1>
+
+      <div class="form-floating mb-3">
+        <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com" autofocus>
+        <label for="floatingInput">Email address</label>
+      </div>
+      <div class="form-floating">
+        <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+        <label for="floatingPassword">Password</label>
+      </div>
+
+      <!-- <div class="form-check text-start my-3">
       <input class="form-check-input" type="checkbox" value="remember-me" id="flexCheckDefault">
       <label class="form-check-label" for="flexCheckDefault">
         Remember me
       </label>
     </div> -->
-    <button class="btn btn-primary w-100 py-2" type="submit">Sign in</button>
-    <p class="mt-5 text-center text-body-secondary">muris.com &copy;2023</p>
-  </form>
-</main>
-@endsection
+      <button class="btn btn-primary w-100 py-2" type="submit">Sign in</button>
+      <p class="mt-5 text-center text-body-secondary">muris.com &copy;2023</p>
+    </form>
+  </main>
+  <script src="{{asset('js/bootstrap.bundle.min.js')}}"></script>
+
+</body>
+
+</htm
