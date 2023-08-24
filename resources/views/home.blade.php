89f45366c08da5e8d0cb972967ebef0eb9fae7c8
murisdotcom <muhrizalsidik>
2023-08-24 22:30:38 +0700

    Menambahkan qr-scanner html5

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,34 @@
 @extends('layouts.main')
 @section('container')
     <h1>Home</h1>
+    {{-- <img src="{{ asset('img/nisa.png') }}" alt="" srcset=""> --}}
     <video src="" id="scanner"></video>
+
+    <script src="{{ asset('js/myScript/html5-qrcode.js') }}" type="text/javascript"></script>
+    <div id="reader" width="600px"></div>
+
+    <script>
+        function onScanSuccess(decodedText, decodedResult) {
+            // handle the scanned code as you like, for example:
+            console.log(`Code matched = ${decodedText}`, decodedResult);
+        }
+
+        function onScanFailure(error) {
+            // handle scan failure, usually better to ignore and keep scanning.
+            // for example:
+            console.warn(`Code scan error = ${error}`);
+        }
+
+        let html5QrcodeScanner = new Html5QrcodeScanner(
+            "reader", {
+                fps: 10,
+                qrbox: {
+                    width: 250,
+                    height: 250
+                }
+            },
+            /* verbose= */
+            false);
+        html5QrcodeScanner.render(onScanSuccess, onScanFailure);
+    </script>
 @endsection
